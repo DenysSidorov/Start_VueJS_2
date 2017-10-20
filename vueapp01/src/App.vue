83b4ec00d4e92a02e73943ac4dc2e3d705655431
Denys Sidorov <000scorpions000@gmail.com>
2017-10-20 18:19:29 +0300

    Initial commit

---
+++ vueapp01/src/App.vue
@@ -0,0 +1,36 @@
+
+<script>
+import HelloWorld from './components/HelloWorld'
+
+export default {
+  name: 'app',
+  data(){
+    return {
+      my: 'First variable, bitch'
+    }
+  },
+  components: {
+    HelloWorld
+  }
+}
+</script>
+
+
+<template>
+  <div id="app">
+    <img src="./assets/logo.png">
+    {{my}}
+    <HelloWorld/>
+  </div>
+</template>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
